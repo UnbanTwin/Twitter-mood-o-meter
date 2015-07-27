a5bbe83a3a5c91e2c4f48fe19e534961ea970856
wizardsambolton <sam99bolton@gmail.com>
2015-07-27 15:34:10 +0100

    i dont know

--- main.js
+++ main.js
@@ -14,16 +14,16 @@ var getTweetCount = function(search) {
       'https://api.twitter.com/1.1/search/tweets.json?q=redflyingllama&count=100&include_entities=false',
       '3359191738-ilVw7893ZDcdLwoNrWkYZx09JHjDyuyG21fOp1w', // Access token
       //you can get it at dev.twitter.com for your own apps
-      'DKNzSY0Ni3zwjq1dIPODpiVKZWdDrjQDjHPyiGsjRU9Gl', 
+      'DKNzSY0Ni3zwjq1dIPODpiVKZWdDrjQDjHPyiGsjRU9Gl',
       //you can get it at dev.twitter.com for your own apps
       function (e, data, res){
-        if (e) console.error(e);        
+        if (e) console.error(e);
         console.log(data);
 		tweet = JSON.parse(data);
 		console.log(tweet.search_metadata.count);
 		console.log(Object.keys(tweet.statuses).length-1);
-        //done();      
-      });    
+        //done();
+      });
 }
 
 var mood
