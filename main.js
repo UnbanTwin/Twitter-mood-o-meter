f97ab4349e196748a6cbba0941fb6ed04dd5a636
Jake Irvine <me@jakeirvine.com>
2015-07-29 12:48:30 +0100

    Code Hedre Have \CCoedxz

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ var client = new Twitter({
   access_token_key: '3359191738-ilVw7893ZDcdLwoNrWkYZx09JHjDyuyG21fOp1w',
   access_token_secret: 'DKNzSY0Ni3zwjq1dIPODpiVKZWdDrjQDjHPyiGsjRU9Gl'
 });
+var currTime = new Date()
+// Dummy tweet to get current tweet ID
+var params = {status:"This is a dummy tweet to get the current tweet ID. Time:" + currTime};
+client.post('statuses/update', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets.id);
+  }
+});
+
+
 // Happy
 var params = {q: "\"happiest\"+OR+\"so+happy\"+OR+\"so+excited\"+OR+\"i'm+happy\"+OR+\"woot\"+OR+\"w00t\"", count: 100};
 client.get('search/tweets', params, function(error, tweets, response){
@@ -24,3 +34,22 @@ client.get('search/tweets', params, function(error, tweets, response){
   }
 });
 // Anger
+
+var params = {q: "\"i+hate\"+OR+\"really+angry\"+OR+\"i+am+mad\"+OR+\"really+hate\"+OR+\"so+angry\"", count: 100};
+client.get('search/tweets', params, function(error, tweets, response){
+  if (!error) {
+    angerTweets = (tweets.statuses.length);
+    console.log("ANGER TWEETS");
+    console.log(angerTweets);
+  }
+});
+// Sadness
+
+var params = {q: "\"i'm+so+sad\"+OR+\"i'm+heartbroken\"+OR+\"i'm+so+upset\"+OR+\"i'm+depressed\"+OR+\"i+can't+stop+crying\"", count: 100};
+client.get('search/tweets', params, function(error, tweets, response){
+  if (!error) {
+    sadTweets = (tweets.statuses.length);
+    console.log("SAD TWEETS");
+    console.log(sadTweets);
+  }
+});
