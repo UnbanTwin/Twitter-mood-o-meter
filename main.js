ec948071eaf4c368f9dc94409bb6f3b3c0428685
Jake Irvine <me@jakeirvine.com>
2015-07-30 10:24:05 +0100

    The yellow submarine

--- main.js
+++ main.js
@@ -12,6 +12,11 @@ var filtering = 10;
 var threshold;
 var uppthresh;
 var totalTweets;
+var threshWindow = 100000000000
+var happy;
+var suprise;
+var anger;
+var sadness;
 // Dummy tweet to get current tweet ID
 var params = {status:"This is a dummy tweet to get the current tweet ID. Time:" + currTime};
 client.post('statuses/update', params, function(error, tweets, response){
@@ -21,56 +26,64 @@ client.post('statuses/update', params, function(error, tweets, response){
     fs.writeFile("data/id.txt", tweetid.toString(), function(err) {
     console.log(tweetid);
     console.log("Tweet file saved");
-    threshold = tweetid - 100000000000 ;
+    threshold = tweetid - threshWindow;
     //uppthresh = tweetid + 100
+    // Happy tweets
     var params = {q: "happiest OR so+happy OR so+excited OR im+happy OR woot OR w00t", count:100, result_type: "recent"};
     params.since_id = threshold
     console.log(threshold);
     client.get('search/tweets', params, function(error, tweets, response){
       if (!error) {
-        console.log(tweets);
+        //console.log(tweets);
         happyTweets = (tweets.statuses.length);
         console.log("HAPPY TWEETS");
         console.log(happyTweets);
-        totalTweets = happyTweets
+        happy = happyTweets
+        // write to file here later
+      }
+    });
+    var params = {q: "wow OR O_o OR can't+believe OR wtf OR unbelievable", count:100, result_type: "recent"};
+    params.since_id = threshold
+    console.log(threshold);
+    client.get('search/tweets', params, function(error, tweets, response){
+      if (!error) {
+        //console.log(tweets);
+        suprisedTweets = (tweets.statuses.length);
+        console.log("SUPRISED TWEETS");
+        console.log(suprisedTweets);
+        suprised = suprisedTweets;
 
       }
     });
+    var params = {q: " i+hate OR really+angry OR i+am+mad OR really+hate OR so+angry ", count:100, result_type: "recent"};
+    params.since_id = threshold
+    console.log(threshold);
+    client.get('search/tweets', params, function(error, tweets, response){
+      if (!error) {
+        //console.log(tweets);
+        angerTweets = (tweets.statuses.length);
+        console.log("ANGRY TWEETS");
+        console.log(angerTweets);
+        anger = angerTweets;
+
+      }
+    });
+    var params = {q: "i'm+so+sad OR i'm+heartbroken OR i'm+so+upset OR i'm+depressed OR i+can't+stop+crying", count:100, result_type: "recent"};
+    params.since_id = threshold
+    console.log(threshold);
+    client.get('search/tweets', params, function(error, tweets, response){
+      if (!error) {
+        //console.log(tweets);
+        sadTweets = (tweets.statuses.length);
+        console.log("SAD TWEETS");
+        console.log(sadTweets);
+        sadness = sadTweets;
+
+      }
+    });
+    // end of the tower of callbacks
     });
   }
 });
 
 // gecode 50.724949, -3.530624
-
-
-// Happy
-
-// Suprise
-var params = {q: "\"wow\"+OR+\"O_o\"+OR+\"can't+believe\"+OR+\"wtf\"+OR+\"unbelievable\"", count: 100, since_id: threshold, max_id: uppthresh};
-client.get('search/tweets', params, function(error, tweets, response){
-  if (!error) {
-    supriseTweets = (tweets.statuses.length);
-    console.log("SUPRISED TWEETS");
-    console.log(supriseTweets);
-  }
-});
-// Anger
-
-var params = {q: "\"i+hate\"+OR+\"really+angry\"+OR+\"i+am+mad\"+OR+\"really+hate\"+OR+\"so+angry\"", count: 100, since_id: threshold, max_id: uppthresh};
-client.get('search/tweets', params, function(error, tweets, response){
-  if (!error) {
-    angerTweets = (tweets.statuses.length);
-    console.log("ANGER TWEETS");
-    console.log(angerTweets);
-  }
-});
-// Sadness
-
-var params = {q: "\"i'm+so+sad\"+OR+\"i'm+heartbroken\"+OR+\"i'm+so+upset\"+OR+\"i'm+depressed\"+OR+\"i+can't+stop+crying\"", count: 100, since_id: threshold, max_id: uppthresh};
-client.get('search/tweets', params, function(error, tweets, response){
-  if (!error) {
-    sadTweets = (tweets.statuses.length);
-    console.log("SAD TWEETS");
-    console.log(sadTweets);
-  }
-});
